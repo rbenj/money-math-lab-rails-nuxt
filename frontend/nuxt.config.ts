5c632af0a4eff19a3c6f7401118c36e0aef77d52
Richard Benjamin <richardbenjamin@me.com>
2026-01-29 14:06:11 -0800

    Configured nuxt and ported css tokens

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  typescript: { strict: true },
+  devtools: { enabled: true },
+  css: ['@/assets/css/main.css'],
+
+  modules: [
+    '@nuxtjs/color-mode'
+  ],
+
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE || 'http://localhost:3001/api/v1',
+    }
+  },
+
+  colorMode: {
+    classSuffix: '',
+    preference: 'system',
+    fallback: 'light',
+  },
 })
