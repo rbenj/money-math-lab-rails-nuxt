c3aec05bc60fff98518add43e3956159d3fa9535
Richard Benjamin <richardbenjamin@me.com>
2026-01-29 14:49:16 -0800

    Added middleware for auth

---
+++ frontend/app/middleware/auth.ts
@@ -0,0 +1,11 @@
+export default defineNuxtRouteMiddleware(async () => {
+  const { user, fetchUser } = useAuth();
+
+  if (user.value === null) {
+    await fetchUser();
+  }
+
+  if (!user.value) {
+    return navigateTo('/login');
+  }
+});
